87220493a761b7426b71e75de301c843b2d75bb2
Onaje Oladuwa <32712596+ooladuwa@users.noreply.github.com>
2021-07-28 09:02:38 -0400

    Initial commit

---
+++ template/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Create React App Template</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
