5ff95195dbaa8230bb0a33c7d81016168dd0f61e
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-07-28 10:38:55 -0400

    created App, Business, BusinessList, and SearchBar components. Added corresponding css files.

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./searchBar.css";
 
+const sortByOptions = {
+  "Best Match": "best_match",
+  "Highest Rated": "rating",
+  "Most Reviewed": "review_count",
+};
+
 const SearchBar = () => {
   return <div></div>;
 };
