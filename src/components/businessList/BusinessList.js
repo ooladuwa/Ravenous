8cdc14c38b70adb29c4239644703ca1fc10c10a3
Onaje Oladuwa <onaje.oladuwa@gmail.com>
2021-07-28 10:29:22 -0400

    created basic app architecture

---
+++ src/components/businessList/BusinessList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Business from "../usiness/Business.js";
+import "./BusinessList.css";
+
+const BusinessList = () => {
+  return (
+    <div class="BusinessList">
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+      <Business />
+    </div>
+  );
+};
+
+export default BusinessList;
